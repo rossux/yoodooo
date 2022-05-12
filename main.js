ffdb7cd83059aebac5176aa8ff8b5618dbc98b61
rossux <rosstmac@gmail.com>
2022-05-12 17:25:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+AOS.init({once: true});
+
+$(document).ready(function(){
+    
+
+  $('.slider-reviews').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1.15,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+
